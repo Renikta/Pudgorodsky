a4544921cc854bfa9bbd23a54b339d3e3e710d6f
Ekaterina Tregub <tregub.es@phystech.edu>
2020-12-02 22:31:23 +0300

    Added key -n

--- myecho.c
+++ myecho.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-    while (--argc > 0)
-        printf("%s%s", *++argv, (argc > 1) ? " " : "");
-    printf("\n");
+    if (argc > 1) {
+
+        char key_n = 1;
+        if (argv[1][0] == '-')
+            key_n = 0;
+        while (--argc > 0)
+            printf("%s%s", *++argv, (argc > 1) ? " " : "");
+        if (key_n)
+            printf("\n");
+
+    }
     return 0;
 }
